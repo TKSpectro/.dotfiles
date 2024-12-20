56f16d52fc36dcdb4b40d7b38a8e0bcced9a1a8c
Tom Käppler <tomkaeppler@web.de>
2024-12-20 17:07:41 +0100

    add ls overwrite to use eza instead of ls

---
+++ fish/.config/fish/functions/ls.fish
@@ -0,0 +1,15 @@
+function ls -d 'eza instead of ls'
+    if type --quiet eza
+        eza --group-directories-first --git --icons $argv
+    else
+        command ls --color=auto $argv
+    end
+end
+
+function ll -d 'alias ls -l'
+    ls -l $argv
+end
+
+function la -d 'alias ls -la'
+    ls -la $argv
+end
